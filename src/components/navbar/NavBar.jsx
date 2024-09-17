0b8323cf1fceb1f267df67da15b718a98794ca83
erratical <105215848+erratical@users.noreply.github.com>
2024-09-17 22:57:51 +0800

    NavBar setup

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+import Links from "./links/Links";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div> Logo </div>
+      <Links />
+    </div>
+  );
+};
+
+export default NavBar;
